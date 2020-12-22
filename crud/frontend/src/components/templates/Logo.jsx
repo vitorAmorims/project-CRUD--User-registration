82874ff0565ea82aa6a58aae63ea256a655d3978
vitorAmorims <vtamorims@gmail.com>
2020-12-22 18:09:44 -0300

    componente logo

--- crud/frontend/src/components/templates/Logo.jsx
+++ crud/frontend/src/components/templates/Logo.jsx
@@ -1,10 +1,13 @@
 import './Logo.css';
 import React from 'react';
+import logo from '../.././assets/imgs/cod3r.png'
 
 export default props => {
     return (
         <aside className="logo">
-            logo
+            <a href="/" className="logo">
+                <img src={logo} alt="logo" />
+            </a>
         </aside>
     )
 }
