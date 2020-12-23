ee614cc68c937156a889456533558b89069ff6d1
vitorAmorims <vtamorims@gmail.com>
2020-12-23 13:21:22 -0300

    componente cadastro de usuario

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios"; //1- importar axios lib hhtp
+import axios from "axios";
 import Main from "../templates/Main";
 
 const headerProps = {
@@ -8,30 +8,34 @@ const headerProps = {
   subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir",
 };
 
-// 2- criar o estado inicial desse componente, caso usuário clicar em cancelar, será restabelecido estado inicial
 const initialState = {
   user: { name: "", email: "" },
   list: [],
 };
-// 3 - endpoint
+
 const baseUrl = "http://localhost:3001/users";
 
 class UserCrud extends React.Component {
-  // 4 - atribuição do estado inicial
   state = { ...initialState };
 
-  // 5 - fn resp por limpar formulario é estabelecer estado inicial de usuário
+  componentDidMount() {
+    axios(baseUrl).then((response) => {
+      this.setState({ list: response.data });
+    });
+    this.renderTable();
+  }
+
   clear = () => {
     this.setState({ user: initialState.user });
   };
 
   // 7 - criar fn getUpdateList
-  getUpdateList = (user) => {
+  getUpdateList = (user, add = true) => {
     //user que obteve do backend
     //gerar lista sem o usuário que passei como parâmetro
     const list = this.state.list.filter((u) => u.id !== user.id);
     //inserindo o usuário que veio do back, como primeiro elemento do array
-    list.unshift(user);
+    if (add) list.unshift(user);
     return list;
   };
 
@@ -40,6 +44,23 @@ class UserCrud extends React.Component {
     const user = { ...this.state.user }; //clonagem de usuário
     user[event.target.name] = event.target.value;
     this.setState({ user });
+  };
+
+  // 6 - fn save para put 'alterar' e post 'incluir'
+  // ********Nota importante:
+  // quando queremos incluir, o usuário não possui id
+  // quando queremos alterar, o id do usuário está setado
+  save = () => {
+    const user = this.state.user;
+    const method = user.id ? "put" : "post";
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+    axios[method](url, user).then((response) => {
+      const list = this.getUpdateList(response.data);
+      this.setState({
+        user: initialState.user,
+        list,
+      });
+    });
   };
 
   // 9 - criar fn renderForm para renderizar os campos do formulário
@@ -91,24 +112,68 @@ class UserCrud extends React.Component {
     );
   }
 
-  // 6 - fn save para put 'alterar' e post 'incluir'
-  // ********Nota importante:
-  // quando queremos incluir, o usuário não possui id
-  // quando queremos alterar, o id do usuário está setado
-  save = () => {
-    const user = this.state.user;
-    const method = user.id ? "put" : "post";
-    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
-    axios[method](url, user).then((response) => {
-      const list = this.getUpdateList(response.data);
-      this.setState({
-        user: initialState.user,
-        list,
-      });
+  // fn load vai atualizar o estado do objeto com user
+  load(user) {
+    this.setState({ user });
+  }
+
+  // fn que remove user do backend e da lista do estado do objeto
+  remove(user) {
+    axios.delete(`${baseUrl}/${user.id}`).then((reponse) => {
+      const list = this.getUpdateList(user, false);
+      this.setState({ list });
     });
   }
+
+  renderRows() {
+    return this.state.list.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button onClick={() => this.load(user)} className="btn btn-warning">
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button
+              onClick={() => this.remove(user)}
+              className="btn btn-danger ml-2"
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  // fn para criar tabela
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>{this.renderRows()}</tbody>
+      </table>
+    );
+  }
+
   render() {
-    return <Main {...headerProps}>{this.renderForm()}</Main>;
+    // console.log(this.state.list);
+    return (
+      <Main {...headerProps}>
+        {this.renderForm()}
+        {this.renderTable()}
+        <strong>{`Total de usuários: ${this.state.list.length}`}</strong>
+      </Main>
+    );
   }
 }
 
