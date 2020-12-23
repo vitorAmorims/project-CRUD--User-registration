f1ce1dc358878da5c91f03976355ba7a3c431430
vitorAmorims <vtamorims@gmail.com>
2020-12-23 14:42:38 -0300

    Adicionando comentarios

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -24,7 +24,6 @@ class UserCrud extends React.Component {
     axios(baseUrl).then((response) => {
       this.setState({ list: response.data });
     });
-    // this.renderTable();
   }
 
   clear = () => {
@@ -33,7 +32,8 @@ class UserCrud extends React.Component {
 
   // 7 - criar fn getUpdateList
   getUpdateList = (user, add = true) => {
-    //user que obteve do backend
+    //param user que obteve do backend
+    //param add flag para atualização
     //gerar lista sem o usuário que passei como parâmetro
     const list = this.state.list.filter((u) => u.id !== user.id);
     //inserindo o usuário que veio do back, como primeiro elemento do array
@@ -65,59 +65,10 @@ class UserCrud extends React.Component {
     });
   };
 
-  // 9 - criar fn renderForm para renderizar os campos do formulário
-  // renderForm() {
-  //   return (
-  //     <div className="form">
-  //       <div className="row">
-  //         <div className="col-12 col-md-6">
-  //           <div className="form group">
-  //             <label htmlFor="name">Nome</label>
-  //             <input
-  //               type="text"
-  //               name="name"
-  //               className="form-control"
-  //               placeholder="Digite o nome..."
-  //               id="name"
-  //               value={this.state.user.name}
-  //               onChange={this.updateField}
-  //             />
-  //           </div>
-  //         </div>
-  //         <div className="col-12 col-md-6">
-  //           <div className="form group">
-  //             <label htmlFor="email">Email</label>
-  //             <input
-  //               type="text"
-  //               name="email"
-  //               className="form-control"
-  //               id="email"
-  //               placeholder="Digite o e-mail..."
-  //               value={this.state.user.email}
-  //               onChange={this.updateField}
-  //             />
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <hr />
-  //       <div className="row">
-  //         <div className="col-12 d-flex justify-content-end">
-  //           <button className="btn btn-primary" onClick={this.save}>
-  //             Salvar
-  //           </button>
-  //           <button className="btn btn-secundary ml-2" onClick={this.clear}>
-  //             Cancelar
-  //           </button>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
   // fn load vai atualizar o estado do objeto com user
   load = (user) => {
     this.setState({ user });
-  }
+  };
 
   // fn que remove user do backend e da lista do estado do objeto
   remove = (user) => {
@@ -125,50 +76,9 @@ class UserCrud extends React.Component {
       const list = this.getUpdateList(user, false);
       this.setState({ list });
     });
-  }
-
-  // renderRows() {
-  //   return this.state.list.map((user) => {
-  //     return (
-  //       <tr key={user.id}>
-  //         <td>{user.id}</td>
-  //         <td>{user.name}</td>
-  //         <td>{user.email}</td>
-  //         <td>
-  //           <button onClick={() => this.load(user)} className="btn btn-warning">
-  //             <i className="fa fa-pencil"></i>
-  //           </button>
-  //           <button
-  //             onClick={() => this.remove(user)}
-  //             className="btn btn-danger ml-2"
-  //           >
-  //             <i className="fa fa-trash"></i>
-  //           </button>
-  //         </td>
-  //       </tr>
-  //     );
-  //   });
-  // }
-
-  // // fn para criar tabela
-  // renderTable() {
-  //   return (
-  //     <table className="table mt-4">
-  //       <thead>
-  //         <tr>
-  //           <th>ID</th>
-  //           <th>Nome</th>
-  //           <th>E-mail</th>
-  //           <th>Ações</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>{this.renderRows()}</tbody>
-  //     </table>
-  //   );
-  // }
+  };
 
   render() {
-    // console.log(this.state.list);
     return (
       <Main {...headerProps}>
         <RenderForm
@@ -178,13 +88,11 @@ class UserCrud extends React.Component {
           name={this.state.user.name}
           email={this.state.user.email}
         />
-        {/* {this.renderForm()} */}
         <RenderTable
           list={this.state.list}
           load={this.load}
           remove={this.remove}
         />
-        {/* {this.renderTable()} */}
         <strong>{`Total de usuários: ${this.state.list.length}`}</strong>
       </Main>
     );
